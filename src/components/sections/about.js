6d5290e9700a5351133611e8207164f380571009
abhivetukuri <abhivetukuri@gmail.com>
2023-09-24 14:40:19 -0400

    modified:   src/components/sections/about.js 	modified:   src/components/sections/hero.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript', 'Python', 'React', 'Node.js', 'Tensorflow', 'AWS', 'Docker', 'SQL'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hi there! My name is Abhi and I'm an aspiring engineer who loves building technology
+              solutions. My passion for coding started back in high school when I used python to
+              provide data analytics for my varsity basketball team. Since then, I've had the
+              opportunity to work on projects spanning cloud computing, machine learning, and full
+              stack development.
             </p>
-
+            <p></p>
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              Some highlights so far include architecting a cloud HPC environment at Cornell's
+              Center of Advanced Computing, and developing ML pipelines and microservices for
+              startups. I enjoy the challenge of ideating creative solutions and seeing them come to
+              life. Outside of work, I also love playing basketball, watching Formula 1, and
+              everything gaming and dc comics related.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,33 +59,22 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>Hey, I am</h1>;
+  const two = <h2 className="big-heading">Abhi Vetukuri.</h2>;
+  const three = <h3 className="big-heading">Driven to innovate, create, and build with tech.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        CS & ORIE undergrad at Cornell. Aspiring engineer with a passion for building innovative
+        solutions at the intersection of technology, finance, and Artificial Intelligence.
+        Experience with applying machine learning and cloud computing to tackle complex problems,
+        and developing full-stack applications with measurable business impact. Excited to
+        continuously learn, collaborate, and make an impact through technology.
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
