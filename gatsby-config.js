b612ea7050a6bec676dfbc420c4ea73f1c2cb394
abhivetukuri <abhivetukuri@gmail.com>
2023-09-24 18:36:49 -0400

    modified:   gatsby-config.js 	modified:   src/components/menu.js 	modified:   src/images/favicons/favicon.ico 	deleted:    static/Abhi Vetukuri SAAL.pdf 	modified:   static/resume.pdf

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,10 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Abhi Vetukuri',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Abhi Vetukuri is a Cornell Undergrad who is looking for new technical challenges and opporunities',
+    siteUrl: 'https://abhivetukuri.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
   },
@@ -23,13 +23,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Abhi Vetukuri',
+        short_name: 'Abhi Vetukuri',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'src/images/favicon.ico',
       },
     },
     `gatsby-plugin-offline`,
